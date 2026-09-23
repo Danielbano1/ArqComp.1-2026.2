4c2e65650ba4c8853aa612c8eb14076b9c0bb649
daniel <daniel@gmail.com>
2026-09-23 17:19:22 -0300

    coloquei checagem de compatibilidade de matrizes

--- matrix_lib.c
+++ matrix_lib.c
@@ -6,18 +6,6 @@
 // Cabeçalhos das minhas bibliotecas
 #include "matrix_lib.h"
 
-// Novos tipos
-typedef struct
-{
-    matrix *m1;
-    matrix *m2;
-    matrix *r;
-    int thread_id;
-} pthread_args;
-
-// Variáveis globais
-#define N_THREADS 16
-
 void matrix_zero_avx(float *values, unsigned long int rows, unsigned long int cols) {
     unsigned long int n = rows * cols;
     unsigned long int i = 0;
@@ -35,7 +23,7 @@ void matrix_zero_avx(float *values, unsigned long int rows, unsigned long int co
 }
 
 /*
-    Ainda falta tratar os possiveis erros. E apagar a versao sem alinhamento de memoria
+    Ainda falta tratar os possiveis erros.
 */
 int scalar_matrix_mult(float scalar_value, matrix *m, matrix *r)
 {
@@ -53,7 +41,7 @@ int scalar_matrix_mult(float scalar_value, matrix *m, matrix *r)
     {
         __m256 v = _mm256_load_ps(&m->values[i]);
         v = _mm256_mul_ps(v, vscalar);
-        _mm256_store_ps(&r->values[i], v); // conferir se a matriz r esta apta para receber os valores
+        _mm256_store_ps(&r->values[i], v); 
     }
 
     return 0;
@@ -76,6 +64,11 @@ int scalar_matrix_mult(float scalar_value, matrix *m, matrix *r)
  */
 int matrix_matrix_mult(matrix *m1, matrix *m2, matrix *r)
 {
+    // Verifica se a multiplicação é possível: colunas de M1 deve ser igual a linhas de M2
+    if (m1->cols != m2->rows) {
+        return 1;
+    }
+    
     int qtd_colunas_m1 = m1->cols;
     int qtd_colunas_m2 = m2->cols;
     int corte_linha_m2 = qtd_colunas_m2 / 8;
